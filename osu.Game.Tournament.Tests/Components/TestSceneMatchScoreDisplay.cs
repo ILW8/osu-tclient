0a97c1edfdb573acc99ca1c162b98b120fb3aa50
Dao Heng Liu <daohengliu@gmail.com>
2025-04-18 14:06:20 +0200

    add live scores to MatchScoreDisplay test scene

--- osu.Game.Tournament.Tests/Components/TestSceneMatchScoreDisplay.cs
+++ osu.Game.Tournament.Tests/Components/TestSceneMatchScoreDisplay.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Logging;
 using osu.Framework.Utils;
 using osu.Game.Tournament.IPC;
 using osu.Game.Tournament.Screens.Gameplay.Components;
@@ -14,27 +16,46 @@ namespace osu.Game.Tournament.Tests.Components
         [Cached(Type = typeof(LegacyMatchIPCInfo))]
         private LegacyMatchIPCInfo legacyMatchInfo = new LegacyMatchIPCInfo();
 
-        public TestSceneMatchScoreDisplay()
+        // public TestSceneMatchScoreDisplay()
+        // {
+        //     legacyMatchInfo.Score1.Value = 727;
+        //
+        //     Add(new TournamentMatchScoreDisplay
+        //     {
+        //         Anchor = Anchor.Centre,
+        //         Origin = Anchor.Centre,
+        //     });
+        // }
+
+        [Test]
+        public void TestScoreUpdate()
         {
-            Add(new TournamentMatchScoreDisplay
+            AddStep("disable cumulative score", () => Ladder.CumulativeScore.Value = false);
+            AddStep("use legacy ipc", () => Ladder.UseLazerIpc.Value = false);
+
+            AddStep("setup screen", () =>
             {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
+                Children = new Drawable[]
+                {
+                    new TournamentMatchScoreDisplay
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    }
+                };
             });
-        }
 
-        protected override void LoadComplete()
-        {
-            base.LoadComplete();
-
-            Scheduler.AddDelayed(() =>
+            AddRepeatStep("update scores", () =>
             {
-                int amount = (int)((RNG.NextDouble() - 0.5) * 10000);
+                int amount = (int)((RNG.NextDouble() - 0.5) * 10_000);
+
+                Logger.Log($"updating score with {amount}");
+
                 if (amount < 0)
                     legacyMatchInfo.Score1.Value -= amount;
                 else
                     legacyMatchInfo.Score2.Value += amount;
-            }, 100, true);
+            }, 100);
         }
     }
 }
