509a41eff063b0ffe2ca9a02d28c619c2c14c303
Dao Heng Liu <daohengliu@gmail.com>
2025-05-04 02:52:01 +0100

    add sliders to control chroma area in showcase screen

--- osu.Game.Tournament/Screens/Showcase/ShowcaseScreen.cs
+++ osu.Game.Tournament/Screens/Showcase/ShowcaseScreen.cs
@@ -18,6 +18,7 @@ namespace osu.Game.Tournament.Screens.Showcase
     public partial class ShowcaseScreen : BeatmapInfoScreen
     {
         private SettingsDropdown<TournamentRound?> roundDropdown = null!;
+        private Drawable chroma = null!;
 
         [BackgroundDependencyLoader]
         private void load(LadderInfo ladder)
@@ -34,13 +35,14 @@ namespace osu.Game.Tournament.Screens.Showcase
                 {
                     Padding = new MarginPadding { Bottom = SongBar.HEIGHT },
                     RelativeSizeAxes = Axes.Both,
-                    Child = new Box
+                    Child = chroma = new Box
                     {
                         // chroma key area for stable gameplay
                         Name = "chroma",
-                        Anchor = Anchor.TopCentre,
-                        Origin = Anchor.TopCentre,
-                        RelativeSizeAxes = Axes.Both,
+                        Anchor = Anchor.BottomCentre,
+                        Origin = Anchor.BottomCentre,
+                        Height = 512,
+                        Y = 0,
                         Colour = new Color4(0, 255, 0, 255),
                     }
                 },
@@ -49,6 +51,34 @@ namespace osu.Game.Tournament.Screens.Showcase
                     Children = new Drawable[]
                     {
                         roundDropdown = new LadderEditorSettings.SettingsRoundDropdown(ladder.Rounds) { LabelText = "Round" },
+                        new SettingsSlider<int>
+                        {
+                            LabelText = "Chroma width",
+                            Current = LadderInfo.ShowcaseChromaWidth,
+                            KeyboardStep = 1,
+                        },
+                        new SettingsSlider<int>
+                        {
+                            LabelText = "Chroma height",
+                            Current = LadderInfo.ShowcaseChromaHeight,
+                            KeyboardStep = 1,
+                        },
+                        new SettingsSlider<int>
+                        {
+                            LabelText = "Chroma offset",
+                            Current = LadderInfo.ShowcaseChromaVerticalOffset,
+                            KeyboardStep = 1,
+                        },
+                        new TourneyButton
+                        {
+                            Text = "Set 16:9 ratio from width",
+                            Action = () => LadderInfo.ShowcaseChromaHeight.Value = LadderInfo.ShowcaseChromaWidth.Value / 16 * 9
+                        },
+                        new TourneyButton
+                        {
+                            Text = "Set 16:9 ratio from height",
+                            Action = () => LadderInfo.ShowcaseChromaWidth.Value = LadderInfo.ShowcaseChromaHeight.Value * 16 / 9
+                        }
                     },
                 }
             });
@@ -57,6 +87,10 @@ namespace osu.Game.Tournament.Screens.Showcase
             {
                 SongBar.Pool = @event.NewValue?.Beatmaps.ToList() ?? [];
             });
+
+            LadderInfo.ShowcaseChromaWidth.BindValueChanged(width => chroma.Width = width.NewValue, true);
+            LadderInfo.ShowcaseChromaHeight.BindValueChanged(height => chroma.Height = height.NewValue, true);
+            LadderInfo.ShowcaseChromaVerticalOffset.BindValueChanged(offset => chroma.Y = -offset.NewValue, true);
         }
 
         protected override void CurrentMatchChanged(ValueChangedEvent<TournamentMatch?> match)
