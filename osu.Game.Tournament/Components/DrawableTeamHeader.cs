ed80d16f07c193f9997e13ea942f8ee99e48ac88
Dao Heng Liu <daohengliu@gmail.com>
2025-05-18 15:59:15 +0100

    use "player" instead of "team" in DrawableTeamHeader

--- osu.Game.Tournament/Components/DrawableTeamHeader.cs
+++ osu.Game.Tournament/Components/DrawableTeamHeader.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Game.Tournament.Models;
 using osuTK;
 
@@ -8,13 +9,25 @@ namespace osu.Game.Tournament.Components
 {
     public partial class DrawableTeamHeader : TournamentSpriteTextWithBackground
     {
+        [Resolved]
+        private LadderInfo ladder { get; set; } = null!;
+
+        private readonly TeamColour colour;
+
         public DrawableTeamHeader(TeamColour colour)
         {
+            this.colour = colour;
             Background.Colour = TournamentGame.GetTeamColour(colour);
 
             Text.Colour = TournamentGame.TEXT_COLOUR;
-            Text.Text = $"Team {colour}".ToUpperInvariant();
             Text.Scale = new Vector2(0.6f);
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            Text.Text = ladder.Use1V1Mode.Value ? $"{colour} player".ToUpperInvariant() : $"Team {colour}".ToUpperInvariant();
         }
     }
 }
